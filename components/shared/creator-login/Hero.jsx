137e9389d143aeab4090cef935f719f90490d4f2
AnishAichh <anishaich4@gmail.com>
2024-06-30 20:16:25 +0530

    small padding change

--- components/shared/creator-login/Hero.jsx
+++ components/shared/creator-login/Hero.jsx
@@ -5,7 +5,7 @@ import './Hero.css'
 const Hero = () => {
     return (
         <div className="text-white p-6 text-left">
-            <div className='bg-secondary py-20 flex flex-col justify-start '>
+            <div className='bg-secondary py-20 flex flex-col justify-start px-6'>
                 <div className='transition transition-container'>
                     <h1 className="text-5xl text-bold font-bold animate-slideInLeft transition-item">WELCOME</h1>
                     <h2 className="text-5xl text-bold text-primary animate-slideInLeft delay-200 transition-item"><span className='text-white'>CREATOR </span>ONBOARD</h2>
