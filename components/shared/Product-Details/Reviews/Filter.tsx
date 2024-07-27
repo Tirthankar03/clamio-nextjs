29a35e6b0485aa496a0e4404504971603a8b56ef
AnishAichh <anishaich4@gmail.com>
2024-07-27 20:40:39 +0530

    filter desktop contents in select radio and imputs changed

--- components/shared/Product-Details/Reviews/Filter.tsx
+++ components/shared/Product-Details/Reviews/Filter.tsx
@@ -1,113 +1,55 @@
 "use client"
 import React from 'react';
 
-const FilterComponent: React.FC = () => {
+const FilterReviews: React.FC = () => {
     return (
-        <div className="p-4 bg-white border border-gray-200  shadow-lg max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg">
+        <div className="p-4 bg-white border border-gray-200 shadow-lg max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg">
             <h3 className="text-xl font-semibold text-gray-800 mb-3">Sort by</h3>
             <select className="w-full bg-gray-100 text-gray-800 text-sm mb-4 py-2 px-3 border border-gray-300 rounded focus:ring-yellow-500 focus:border-yellow-500">
-                <option value="custom">Custom</option>
-                <option value="newest-oldest">Price: Newest to Oldest</option>
-                <option value="price-high-low">Price: Oldest to Newest</option>
-                <option value="price-low-high">Price: Low to High</option>
-                <option value="price-high-low">Price: High to Low</option>
+                <option value="highest-rated">Highest Rated</option>
+                <option value="most-recent">Most Recent</option>
+                <option value="least-recent">Least Recent</option>
             </select>
 
-            <h3 className="text-xl font-semibold text-gray-800 mb-3">Minimum price</h3>
-            <input type="number" name="minPrice" placeholder="$0" className="w-full bg-gray-100 text-gray-800 text-sm mb-4 py-2 px-3 border border-gray-300 rounded focus:ring-yellow-500 focus:border-yellow-500" />
+            <h3 className="text-xl font-semibold text-gray-800 mb-3">Minimum Rating</h3>
+            <input type="number" name="minRating" placeholder="1" className="w-full bg-gray-100 text-gray-800 text-sm mb-4 py-2 px-3 border border-gray-300 rounded focus:ring-yellow-500 focus:border-yellow-500" />
 
-            <h3 className="text-xl font-semibold text-gray-800 mb-3">Maximum price</h3>
-            <input type="number" name="maxPrice" placeholder="∞" className="w-full bg-gray-100 text-gray-800 text-sm mb-4 py-2 px-3 border border-gray-300 rounded focus:ring-yellow-500 focus:border-yellow-500" />
+            <h3 className="text-xl font-semibold text-gray-800 mb-3">Maximum Rating</h3>
+            <input type="number" name="maxRating" placeholder="5" className="w-full bg-gray-100 text-gray-800 text-sm mb-4 py-2 px-3 border border-gray-300 rounded focus:ring-yellow-500 focus:border-yellow-500" />
 
-            <h3 className="text-xl font-semibold text-gray-800 mb-3">Rating</h3>
+            <h3 className="text-xl font-semibold text-gray-800 mb-3">Review Type</h3>
             <div className="mb-4 text-sm text-gray-600">
                 <label className="block mb-2">
-                    <input type="radio" name="rating" value="5" className="mr-2 text-yellow-500" />
-                    5 stars and up
+                    <input type="radio" name="reviewType" value="all" className="mr-2 text-yellow-500" defaultChecked />
+                    All Reviews
                 </label>
                 <label className="block mb-2">
-                    <input type="radio" name="rating" value="4" className="mr-2 text-yellow-500" />
-                    4 stars and up
+                    <input type="radio" name="reviewType" value="with-images" className="mr-2 text-yellow-500" />
+                    With Images
                 </label>
                 <label className="block mb-2">
-                    <input type="radio" name="rating" value="3" className="mr-2 text-yellow-500" />
-                    3 stars and up
-                </label>
-                <label className="block mb-2">
-                    <input type="radio" name="rating" value="2" className="mr-2 text-yellow-500" />
-                    2 stars and up
-                </label>
-                <label className="block mb-2">
-                    <input type="radio" name="rating" value="1" className="mr-2 text-yellow-500" />
-                    1 star and up
+                    <input type="radio" name="reviewType" value="with-comments" className="mr-2 text-yellow-500" />
+                    With Comments
                 </label>
             </div>
 
-            <h3 className="text-xl font-semibold text-gray-800 mb-3">Showing</h3>
+            <h3 className="text-xl font-semibold text-gray-800 mb-3">Categories</h3>
             <div className="mb-4 text-sm text-gray-600">
                 <label className="block mb-2">
-                    <input type="checkbox" value="3d model" className="mr-2 text-yellow-500" />
-                    3d model (2740)
+                    <input type="checkbox" value="positive" className="mr-2 text-yellow-500" />
+                    Positive
                 </label>
                 <label className="block mb-2">
-                    <input type="checkbox" value="vrchat" className="mr-2 text-yellow-500" />
-                    vrchat (2630)
+                    <input type="checkbox" value="negative" className="mr-2 text-yellow-500" />
+                    Negative
                 </label>
                 <label className="block mb-2">
-                    <input type="checkbox" value="pbr" className="mr-2 text-yellow-500" />
-                    pbr (1497)
-                </label>
-                <label className="block mb-2">
-                    <input type="checkbox" value="game assets" className="mr-2 text-yellow-500" />
-                    game assets (1142)
-                </label>
-                <label className="block mb-2">
-                    <input type="checkbox" value="vrchat avatar" className="mr-2 text-yellow-500" />
-                    vrchat avatar (1029)
-                </label>
-            </div>
-
-            <h3 className="text-xl font-semibold text-gray-800 mb-3">Category</h3>
-            <div className="mb-4 text-sm text-gray-600">
-                <label className="block mb-2">
-                    <input type="checkbox" value="education" className="mr-2 text-yellow-500" />
-                    Education
-                </label>
-                <label className="block mb-2">
-                    <input type="checkbox" value="entertainment" className="mr-2 text-yellow-500" />
-                    Entertainment
-                </label>
-                <label className="block mb-2">
-                    <input type="checkbox" value="technology" className="mr-2 text-yellow-500" />
-                    Technology
-                </label>
-                <label className="block mb-2">
-                    <input type="checkbox" value="lifestyle" className="mr-2 text-yellow-500" />
-                    Lifestyle
-                </label>
-            </div>
-
-            <h3 className="text-xl font-semibold text-gray-800 mb-3">File Type</h3>
-            <div className="mb-4 text-sm text-gray-600">
-                <label className="block mb-2">
-                    <input type="checkbox" value="pdf" className="mr-2 text-yellow-500" />
-                    PDF
-                </label>
-                <label className="block mb-2">
-                    <input type="checkbox" value="zip" className="mr-2 text-yellow-500" />
-                    ZIP
-                </label>
-                <label className="block mb-2">
-                    <input type="checkbox" value="image" className="mr-2 text-yellow-500" />
-                    Image
-                </label>
-                <label className="block mb-2">
-                    <input type="checkbox" value="video" className="mr-2 text-yellow-500" />
-                    Video
+                    <input type="checkbox" value="neutral" className="mr-2 text-yellow-500" />
+                    Neutral
                 </label>
             </div>
         </div>
     );
 };
 
-export default FilterComponent;
+export default FilterReviews;
